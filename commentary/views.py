d60f144d8fb6f5910e5ba12342cffa509715e9b4
Denys Tereshchuk <terrya@ukr.net>
2023-06-05 12:35:19 +0400

    Added CommentaryViewSet in views.py

--- commentary/views.py
+++ commentary/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from commentary.models import Commentary
+from commentary.serializers import CommentarySerializer
+
+
+class CommentaryViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentarySerializer
+    queryset = Commentary.objects.all()
