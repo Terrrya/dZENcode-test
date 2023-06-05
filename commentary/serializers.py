c4bfd7a2057bf8a70f5987a6ce65e3a90849fe85
Denys Tereshchuk <terrya@ukr.net>
2023-06-05 18:02:56 +0400

    Added CreateCommentarySerializer to commentary.serializers

--- commentary/serializers.py
+++ commentary/serializers.py
@@ -6,10 +6,10 @@ from commentary.models import Commentary
 class CommentarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Commentary
-        fields = (
-            "id",
-            "created_at",
-            "user",
-            "parent_commentary",
-            "body",
-        )
+        fields = ("id", "created_at", "user", "parent_commentary", "body")
+
+
+class CreateCommentarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Commentary
+        fields = ("id", "parent_commentary", "body")
