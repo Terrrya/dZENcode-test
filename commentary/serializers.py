d76fb00c89cb7a3481552bf0d6f97cf1c68972b1
Denys Tereshchuk <terrya@ukr.net>
2023-06-06 00:45:35 +0400

    Added validation commentary text to CreateCommentarySerializer in commentary.serializer

--- commentary/serializers.py
+++ commentary/serializers.py
@@ -1,4 +1,8 @@
+import re
+
+from bs4 import BeautifulSoup
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from commentary.models import Commentary
 
@@ -9,7 +13,26 @@ class CommentarySerializer(serializers.ModelSerializer):
         fields = ("id", "created_at", "user", "parent_commentary", "body")
 
 
-class CreateCommentarySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Commentary
-        fields = ("id", "parent_commentary", "body")
+class CreateCommentarySerializer(CommentarySerializer):
+    created_at = serializers.DateTimeField(read_only=True)
+    user = serializers.SlugRelatedField(
+        slug_field="id", read_only=True, many=False
+    )
+
+    def validate(self, attrs: dict) -> dict:
+        data = super().validate(attrs)
+        commentary_text = data["body"]
+
+        if re.search(r"<(?!\/?(?:strong|i|code|a))[^>]*>", commentary_text):
+            raise ValidationError(
+                {
+                    "body": "you should use only <strong>, <i>, <code>, "
+                    "<a> tags"
+                }
+            )
+
+        soup = BeautifulSoup(commentary_text, "html.parser")
+        if str(soup) != commentary_text:
+            raise ValidationError({"body": "You should use only html style"})
+
+        return data
