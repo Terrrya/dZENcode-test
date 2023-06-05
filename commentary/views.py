bc30dd861c36681c3960aad59591bf4c8d7a46b5
Denys Tereshchuk <terrya@ukr.net>
2023-06-05 17:48:43 +0400

    Added permissions.py with custom permission in commentary app & set up IsAuthenticatedOrIfNonAuthenticatedReadOnly permission to CommentaryViewSet

--- commentary/views.py
+++ commentary/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 
 from commentary.models import Commentary
+from commentary.permissions import IsAuthenticatedOrIfNonAuthenticatedReadOnly
 from commentary.serializers import CommentarySerializer
 
 
 class CommentaryViewSet(viewsets.ModelViewSet):
     serializer_class = CommentarySerializer
     queryset = Commentary.objects.all()
+    permission_classes = (IsAuthenticatedOrIfNonAuthenticatedReadOnly,)
