ecaf094b386604e6096d448ac137165ba09df52d
Denys Tereshchuk <terrya@ukr.net>
2023-06-05 12:18:35 +0400

    Added base Commentary model in models.py

--- commentary/models.py
+++ commentary/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Commentary(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="commentaries"
+    )
+    parent_commentary = models.ForeignKey(
+        "self", on_delete=models.CASCADE, related_name="child_commentaries"
+    )
+    body = models.TextField()
