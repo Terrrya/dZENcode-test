398fc9e975da5d9317b85e5ec7ef6c8b75574836
Denys Tereshchuk <terrya@ukr.net>
2023-06-05 18:23:11 +0400

    Updated get_serializer_class & perform_create methods for CommentaryViewSet

--- commentary/views.py
+++ commentary/views.py
@@ -1,11 +1,24 @@
+from typing import Type
+
 from rest_framework import viewsets
+from rest_framework.serializers import ModelSerializer
 
 from commentary.models import Commentary
 from commentary.permissions import IsAuthenticatedOrIfNonAuthenticatedReadOnly
-from commentary.serializers import CommentarySerializer
+from commentary.serializers import (
+    CommentarySerializer,
+    CreateCommentarySerializer,
+)
 
 
 class CommentaryViewSet(viewsets.ModelViewSet):
-    serializer_class = CommentarySerializer
     queryset = Commentary.objects.all()
     permission_classes = (IsAuthenticatedOrIfNonAuthenticatedReadOnly,)
+
+    def get_serializer_class(self, *args, **kwargs) -> Type[ModelSerializer]:
+        if self.action == "create":
+            return CreateCommentarySerializer
+        return CommentarySerializer
+
+    def perform_create(self, serializer: CreateCommentarySerializer) -> None:
+        serializer.save(user=self.request.user)
