bd79eb5ba0db7997577321591fc9f988c3223faa
Denys Tereshchuk <terrya@ukr.net>
2023-06-05 14:47:39 +0400

    Updated method create & update for encrypt password in UserSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -21,3 +21,18 @@ class UserSerializer(serializers.ModelSerializer):
             "is_staff",
         )
         extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, set the password correctly and return it"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
